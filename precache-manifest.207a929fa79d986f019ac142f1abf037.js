31a079b7434d791e97be36b4687b4e6f114f9418
Gaurav2146 <61244204+Gaurav2146@users.noreply.github.com>
2020-07-02 21:58:37 +0530

    Updates

---
+++ precache-manifest.207a929fa79d986f019ac142f1abf037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c0e65d255313c216100953db7ffd7a",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "173c0eb291fe62c975bf",
+    "url": "/reactdemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/reactdemo/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdemo/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173c0eb291fe62c975bf",
+    "url": "/reactdemo/static/js/main.4d008c94.chunk.js"
+  },
+  {
+    "revision": "72396104d2531a6299e7",
+    "url": "/reactdemo/static/js/runtime-main.5ec03059.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
